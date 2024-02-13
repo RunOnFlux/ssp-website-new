002d0362218f0a1c7c4c0152260066ba60d7ee6c
TheTrunk <tadeas.kmenta@zel.cash>
2024-02-13 16:18:47 +0700

    fix

--- components/homePage/HomePage.js
+++ components/homePage/HomePage.js
@@ -22,7 +22,7 @@ export function HomePage(){
                     <div className={styles.card1}>
                         <h3>Revolutionize </h3>
                         <h6>your wallet with <span>ssp</span></h6>
-                        <p>SSP Wallet ensures user ownership with private keys back in their hands, emphasizing Security, Simplicity, and Powerful features. It pioneers cutting-edge encryption and mobile-integrated multi-signature mechanisms for a strong two-key authentication process. With a commitment to not storing any data information, SSP guarantees a secure environment. As a decentralized, open-source project, SSP evolves globally, empowering users to shape this Web3 wallet. Available as a Chrome extension, it seamlessly integrates with major DApp platforms..</p>
+                        <p>SSP Wallet ensures user ownership with private keys back in your hands, emphasizing Security, Simplicity, and Powerful features. It pioneers cutting-edge encryption and mobile-integrated multi-signature mechanisms for a strong two-key authentication process. With a commitment to not storing any data information, SSP guarantees a secure environment. As a decentralized, open-source project, SSP evolves globally, empowering users to shape this Web3 wallet. Available as a Chrome extension, it seamlessly integrates with major DApp platforms..</p>
                     </div>
                         <div className={styles.cardPicture}>
                         {/* <Image alt="picture" src={picture1} /> */}
